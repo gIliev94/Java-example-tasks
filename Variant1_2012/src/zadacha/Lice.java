94c4234e706c4884a4c621098683dfafc6b7561a
Georgi Iliev <neokaozzz@gmail.com>
2015-04-12 19:42:51 +0300

    Second example task

---
+++ Variant1_2012/src/zadacha/Lice.java
@@ -0,0 +1,16 @@
+package zadacha;
+
+public class Lice {
+	 String ime;
+	 String EGN;
+	 
+	 public Lice(){
+		 
+	 }
+	 
+	 public Lice(String ime,String EGN){
+	this.ime=ime;	 
+	this.EGN=EGN;
+	 }
+
+}
