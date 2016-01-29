ef4767e4bbe3c756613c9b41ca5d07f9c3596657
Georgi Iliev <neokaozzz@gmail.com>
2016-01-29 23:43:53 +0200

    Refactors - method extraction/renames

--- Sandbox/src/sirma/tasks/letterfrequency/LetterFrequencyFinder.java
+++ Sandbox/src/sirma/tasks/letterfrequency/LetterFrequencyFinder.java
@@ -6,61 +6,64 @@ import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.TreeMap;
 
 public final class LetterFrequencyFinder {
 
     public static final int PRINT_LIMIT = 20;
-    public static final int MAXIMUM_HASHTAGS = 20;
+    public static final int MAXIMUM_ALLOWED_HASHTAGS_ = 20;
 
     public static void main(String[] args) {
 
-	char[] characters;
-	Integer frequency;
 	Map<Character, Integer> characterMap = new TreeMap<>();
 
 	System.out.println("Input text here:");
+	Scanner input = new Scanner(System.in);
 
-	try (Scanner input = new Scanner(System.in)) {
+	try {
 
-	    characters = input.nextLine().toUpperCase().toCharArray();
+	    String text = input.nextLine().toUpperCase();
 
-	    for (Character c : characters) {
-		frequency = characterMap.get(c);
+	    determineFrequencies(characterMap, text);
 
-		if (Character.isLetter(c)) {
-		    characterMap.put(c, (frequency == null) ? 1 : frequency + 1);
-		}
-	    }
-
-	    Map<Character, Integer> sortedMap = sortValuesDescending(characterMap);
+	    Map<Character, Integer> sortedMap = getSortedByValuesDescending(characterMap);
 
 	    System.out.println("\nMost frequent letters are:");
-	    limitPrinting(sortedMap);
+	    doLimitedPrinting(sortedMap);
 
-	} catch (NoSuchElementException nse) {
-	    System.err.println("NO LINE WAS FOUND!\n");
-	    nse.printStackTrace();
-	} catch (IllegalStateException ise) {
-	    System.err.println("THE SCANNER IS CLOSED!\n");
-	    ise.printStackTrace();
+	} finally {
+	    input.close();
 	}
     }
 
-    private static <K, V extends Comparable<? super V>> Map<Character, Integer> sortValuesDescending(
+    private static void determineFrequencies(Map<Character, Integer> characterMap, String text) {
+	char[] characters;
+	Integer frequency;
+	characters = text.toCharArray();
+
+	for (Character c : characters) {
+	    frequency = characterMap.get(c);
+
+	    if (Character.isLetter(c)) {
+		characterMap.put(c, (frequency == null) ? 1 : frequency + 1);
+	    }
+	}
+    }
+
+    private static <K, V extends Comparable<? super V>> Map<Character, Integer> getSortedByValuesDescending(
 	    Map<Character, Integer> map) {
+
 	List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
 
 	Collections.sort(list, new Comparator<Map.Entry<Character, Integer>>() {
 	    @Override
-	    public int compare(Map.Entry<Character, Integer> o1, Map.Entry<Character, Integer> o2) {
-		return (o2.getValue()).compareTo(o1.getValue());
+	    public int compare(Map.Entry<Character, Integer> obj1, Map.Entry<Character, Integer> obj2) {
+		return (obj2.getValue()).compareTo(obj1.getValue());
 	    }
 	});
 
-	map = new LinkedHashMap<>();
+	map = new LinkedHashMap<>(map.size());
 
 	for (Map.Entry<Character, Integer> entry : list) {
 	    map.put(entry.getKey(), entry.getValue());
@@ -69,14 +72,14 @@ public final class LetterFrequencyFinder {
 	return map;
     }
 
-    private static void limitPrinting(Map<Character, Integer> outMap) {
+    private static void doLimitedPrinting(Map<Character, Integer> outMap) {
 
 	int printCounter = 0;
-	int highestCharacterFrequency = 0;
+	int highestFrequency = 0;
 	int currentFrequency = 0;
 	int currentHashtags = 0;
 
-	highestCharacterFrequency = outMap.values().iterator().next();
+	highestFrequency = outMap.values().iterator().next();
 
 	for (Map.Entry<Character, Integer> entry : outMap.entrySet()) {
 
@@ -86,8 +89,8 @@ public final class LetterFrequencyFinder {
 	    System.out.print(entry.getKey() + " " + entry.getValue() + " ");
 
 	    currentFrequency = entry.getValue().intValue();
-	    currentHashtags = (currentFrequency == highestCharacterFrequency ? MAXIMUM_HASHTAGS
-		    : (currentFrequency * MAXIMUM_HASHTAGS) / highestCharacterFrequency);
+	    currentHashtags = (currentFrequency == highestFrequency ? MAXIMUM_ALLOWED_HASHTAGS_
+		    : (currentFrequency * MAXIMUM_ALLOWED_HASHTAGS_) / highestFrequency);
 
 	    for (int i = 1; i <= currentHashtags; i++) {
 		System.out.print("#");
