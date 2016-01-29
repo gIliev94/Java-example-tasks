7b68d56164a817105f1680843229b8c8cda1c3b9
Georgi Iliev <neokaozzz@gmail.com>
2016-01-30 01:07:22 +0200

    Description + refactoring

--- Sandbox/src/companyTaskSolutions/ArrayValueFrequency.java
+++ Sandbox/src/companyTaskSolutions/ArrayValueFrequency.java
@@ -1,29 +1,59 @@
 package companyTaskSolutions;
 
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
+/**
+ * Problem description:
+ * <p>
+ * Determine whether there is a duplicate value in an array of Integers - print
+ * '0'if a duplicate is found, do nothing if .
+ * </p>
+ *
+ * @author Georgi Iliev
+ *
+ */
 public class ArrayValueFrequency {
+
+    public static final int MAX = 50;
+    public static final int MIN = 0;
 
     public static void main(String[] args) {
 
-	int[] array = { 3, 4, 5, 6, 7, 10 };
+	int[] array = initArray();
 
-	if (hasNoDuplicates(array)) {
+	if (!hasDuplicates(array)) {
 	    System.out.print("0");
 	}
     }
 
-    public static boolean hasNoDuplicates(int[] arr) {
+    private static int[] initArray() {
+	Random random = new Random();
+
+	int randomLenght = random.nextInt((MAX - MIN) + 1) + MIN;
+	int[] arr = new int[randomLenght];
+
+	int randomNumber = 0;
+
+	for (int i = 0; i < arr.length; i++) {
+	    randomNumber = random.nextInt((MAX - MIN) + 1) + MIN;
+	    arr[i] = randomNumber;
+	}
+
+	return arr;
+    }
+
+    private static boolean hasDuplicates(int[] arr) {
 
 	Set<Integer> uniques = new HashSet<>();
 
 	for (int i = 0; i < arr.length; i++) {
 	    if (!uniques.add(arr[i])) {
-		return false;
+		return true;
 	    }
 	}
-	return true;
+	return false;
     }
 
 }
